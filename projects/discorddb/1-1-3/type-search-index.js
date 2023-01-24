4e2410bb18fe27592840ffd7d3816ac3407f2239
boole <samarthupadhya2309@gmail.com>
2023-01-23 21:47:44 -0500

    reworking website theme updated discorddb database documentation added articles

---
+++ projects/discorddb/1-1-3/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"discorddb","l":"DatabaseManager"},{"p":"discorddb","l":"DatabaseObject"},{"p":"<Unnamed>","l":"DatabaseObjectTests"},{"p":"<Unnamed>","l":"ManagerTests"},{"p":"<Unnamed>","l":"TestRunner"}];updateSearchResults();
