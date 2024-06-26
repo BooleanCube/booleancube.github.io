57f1e58155205dc2e9a1b4c60fb63b8b5e61acd3
boole <samarthupadhya2309@gmail.com>
2024-06-25 21:40:39 -0400

    update discorddb documentation to v2.1.3

--- projects/discorddb/2-1-3/package-search-index.js
+++ projects/discorddb/2-1-3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"discorddb.jsondb"},{"l":"discorddb.sqlitedb"},{"l":"jsondb"},{"l":"sqlitedb"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"discorddb.jsondb"},{"l":"discorddb.sqlitedb"}];updateSearchResults();
